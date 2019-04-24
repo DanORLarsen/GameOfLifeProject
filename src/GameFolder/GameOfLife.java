709379ec4f29baa6914f1c3a6141e2daa9daaf39
DanORLarsen <42993978+DanORLarsen@users.noreply.github.com>
2019-04-24 19:36:32 +0100

    Cant figure out how to improve Performance...

--- src/GameFolder/GameOfLife.java
+++ src/GameFolder/GameOfLife.java
@@ -5,22 +5,15 @@ import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.concurrent.Task;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
-
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class GameOfLife extends Application {
 Timeline timeline = null;
@@ -169,7 +162,6 @@ public void start(Stage stage) {
                     circle.setFill(Color.WHITE);
                 }
                 grid.add(circle, c, r);
-//Check if u should remove from pane from grif and then add after?
             }
         }
     }
